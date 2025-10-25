c234fa7b45df08c8ca63f4a5b2d56e9e87cdf1b8
jacquesjulies <jacquesjulies007@gmail.com>
2025-10-25 13:55:01 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+
+    document.getElementById("scrollToCasinoSection").addEventListener("click", function() {
+        document.querySelector(".casino-section").scrollIntoView({ behavior: "smooth" });
+    });
+
+   
